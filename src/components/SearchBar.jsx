b5b8bc47fa082d273dd311d0f42fb3b6f2f8f130
Chuacx13 <chuachongxuan@gmail.com>
2024-02-22 02:22:21 +0800

    Completed Home Page and Search Bar design

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { UserAuth } from '../utils/auth';
 
-const Searchbar = () => {
+const SearchBar = () => {
+
+  const { user } = UserAuth();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("hi")
+  };
+
   return (
-    <div>Searchbar</div>
-  )
+    <form onSubmit={handleSubmit} className="flex mt-8" >
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
+        placeholder="Sign in to use our website"
+        className="border border-gray-800 rounded py-2 px-2 md:w-96 focus:outline-none focus:ring focus:border-blue-300"
+      />
+      <button type="submit" className="bg-gray-800 text-white hover:bg-blue-1000 py-2 px-4 rounded" disabled={!user}>
+        Search
+      </button>
+    </form>
+  );
 }
 
-export default Searchbar
+export default SearchBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import lyricsvault_logo from '../assets/lyricsvault_logo.png'
+import SearchBar from '../components/SearchBar'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="flex flex-col items-center justify-center">
+      <img src={lyricsvault_logo} alt="Logo" className="mt-20"/>
+      <h1 className="text-xl font-bold mx-20 text-center">
+        Unlock the melody to your mystery tune! Dive into LyricsVault and uncover songs with just its lyrics.
+      </h1>
+      <SearchBar/>
+    </div>
   )
 }
 
